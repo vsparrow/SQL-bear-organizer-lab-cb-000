a9d279134ce623ba5f1c92ea4ba4a9b5f0d41837
vsparrow <khan@vsparrow.com>
2018-02-07 02:58:17 -0500

    Completed create.sql and insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES
+(1,"Thom",12,"M","Brown","nice",1),
+(2,"Mark",5,"M","Black","grouchy",1),
+(3,"Nance",8,"F","White","sweet",1),
+(4,"Mari",32,"F","Gray","hungry",1),
+(5,"Bob",22,"M","Red","cool",1),
+(6,"Bobo",13,"M","Brown","sweet",1),
+(7,Null,2,"F","Black","cute",1),
+(8,"Shawna",45,"F","Gray","pleasent",1);
